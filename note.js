b50eae3544cd1d2479a0416037dd7bfb53bf128c
Dionna <businessladythick@yahoo.com>
2024-02-22 19:33:33 -0500

    i changed the way the code was written

--- note.js
+++ note.js
@@ -9,6 +9,7 @@ const n4 = 5;
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
 console.log(isSum50);
+console.log('----------')
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
@@ -16,12 +17,14 @@ console.log(isSum50);
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 console.log(isTwoOdd);
+console.log('----------')
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 console.log(isOver25);
+console.log('---------')
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -29,6 +32,7 @@ console.log(isOver25);
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 console.log(isUnique);
+console.log('----------')
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
@@ -36,24 +40,36 @@ console.log(isUnique);
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 console.log(isValid);
+console.log('----------')
 
 // If all numbers are divisible by 5
 // cache the result in a variable
-let allDivisibleBy5 = true
-console.log(allDivisibleBy5);
-// 
+// let allDivisibleBy5 = true
+// console.log(allDivisibleBy5);
+ 
+
 // If the first number is larger than the last
 // cache the results in a variable
 // const firstNumber = 10
 // const lastNumber = 5
 // let result = true
 // console.log(firstNumber > lastNumber);
-// 
+
+
 // Subtract the first number from the second number
-const firstNumber = 10
-const secondNumber = 15
-const = secondNumber - firstNumber;
-console.log(value);
-// 
+const sum = n2 - n1
+console.log(sum);
+console.log('----------')
+
+
 //Multiply the result by the third number 
+const multiple = 5 * n3
+console.log(multiple);
+console.log('----------')
+
+// Find the remainder of dividing the result byu the fourth number
+const divide = 100 / n3
+console.log(divide);
+console.log('----------')
+
 
