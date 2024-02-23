390f075da07b0adfe0ee8dc86191493d0291b206
Dionna <businessladythick@yahoo.com>
2024-02-22 19:35:03 -0500

    same thing

--- note.js
+++ note.js
@@ -8,7 +8,7 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-console.log(isSum50);
+console.log('The four numbers total 50 according to the provided criteria:${isSum50}');
 console.log('----------')
 
 // Check two: at least two odd numbers
@@ -16,14 +16,14 @@ console.log('----------')
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
-console.log(isTwoOdd);
+console.log('At least two numbers are odd according to the provided criteria:${isTwoOdd}');
 console.log('----------')
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-console.log(isOver25);
+const isUnder25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+console.log('There is not any numbers over 25 according to the provided criteria:${isUnder25}');
 console.log('---------')
 
 // Check four: all unique numbers
@@ -31,7 +31,7 @@ console.log('---------')
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
-console.log(isUnique);
+console.log('The four numbers are unique according to the provided criteria:${isUnique}');
 console.log('----------')
 
 // Here, we put the results into a single variable 
@@ -39,13 +39,13 @@ console.log('----------')
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
-console.log(isValid);
+console.log('The four numbers are under 25 according to the provided criteria:${isValid}');
 console.log('----------')
 
 // If all numbers are divisible by 5
 // cache the result in a variable
-// let allDivisibleBy5 = true
-// console.log(allDivisibleBy5);
+let allDivisibleBy5 = true
+console.log('All four numbers are divisible by 5 according to the provided criteria:${allDivisibleBy5}');
  
 
 // If the first number is larger than the last
@@ -58,18 +58,22 @@ console.log('----------')
 
 // Subtract the first number from the second number
 const sum = n2 - n1
-console.log(sum);
+console.log('The equasion is sum according to the provided criteria:${sum}');
 console.log('----------')
 
 
 //Multiply the result by the third number 
 const multiple = 5 * n3
-console.log(multiple);
+console.log('The multiple equals 100 according to the provided criteria:${5*n3}');
 console.log('----------')
 
 // Find the remainder of dividing the result byu the fourth number
 const divide = 100 / n3
-console.log(divide);
+console.log('The division equals e according to the provided criteria:${100/n3}');
 console.log('----------')
+
+// Change the way that isOver25 calculates so that we
+//do not need to use the NOT operator(!) in other logic
+//comparisons. Rename the variables as appropriate
 
 
